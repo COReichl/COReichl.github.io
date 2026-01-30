071e7ab962b4c8d3a24618541db1a257fa5be75c
COReichl <reichlcolin@gmail.com>
2026-01-29 21:20:32 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
-console.log('testing')
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const messages = ['Good Morning! Welcome to My Page', 'Good Afternoon! Welcome to My Page', 'Good Evening! Welcome to My Page', 'Welcome to My Page']
+let i = 3
+
+if(isMorning === true) {i=0}
+else if(isAfternoon === true) {i=1}
+else  {i=2}
+
+const list = document.querySelector('#welcome')
+
+const h2 = document.createElement('h2')
+list.append(h2)
+
+h2.innerHTML = messages[i]
